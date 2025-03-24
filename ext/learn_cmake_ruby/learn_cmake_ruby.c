3e3c19b8d2f7a3acb17e8c90df1ed7f105772c65
Hou Yuanjie <houyuanjie555@qq.com>
2025-03-24 18:40:23 +0800

    binding: library dependency

--- ext/learn_cmake_ruby/learn_cmake_ruby.c
+++ ext/learn_cmake_ruby/learn_cmake_ruby.c
@@ -1,6 +1,20 @@
 #include <ruby.h>
+#include <hello.h>
+
+VALUE rb_mLearnCmakeRuby;
+VALUE rb_mHello;
+
+VALUE rb_hello_get_message(VALUE self)
+{
+    char cstr[100];
+    get_message(cstr, sizeof(cstr) / sizeof(char));
+    VALUE str = rb_str_new_cstr(cstr);
+    return str;
+}
 
 void Init_learn_cmake_ruby()
 {
-    rb_eval_string("puts %q(Hello Ruby!)");
+    rb_mLearnCmakeRuby = rb_define_module("LearnCmakeRuby");
+    rb_mHello = rb_define_module_under(rb_mLearnCmakeRuby, "Hello");
+    rb_define_module_function(rb_mHello, "message", rb_hello_get_message, 0);
 }
